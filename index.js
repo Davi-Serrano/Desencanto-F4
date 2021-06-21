9c39512d7e2115fa0c8835a9705834759e6fa229
Davi Serrano <daviscardoso2901@gmail.com>
2021-06-21 01:02:38 -0300

    terminado todo o design

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ function talkLuci() {
     alert("Ahhh Morre Diabo")
     
 }
+
+
+function talkTibine() {
+    alert("Eu to muito bêbada")
+    
+}
+
+
+
+function talkElfo() {
+    alert("Alguém viu a tibine por aí?")
+    
+}
