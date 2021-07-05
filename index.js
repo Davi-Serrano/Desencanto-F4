284a0637a4eb35473fcc42b5d2f6f226d9339ee9
Davi Serrano <daviscardoso2901@gmail.com>
2021-07-05 01:06:37 -0300

    chat aleatório criado

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 function talkLuci() {
-    alert("Ahhh Morre Diabo")
-    
+    const speak = ["Ahh!!! Morre Diabo!!",
+        "Eu não sou um gato. ",
+        "Miauu!!",
+        "Depois da uma passadinha lá no meu bar bebê, tem umas fadas que vão adorar você...",
+        "Rapaz!! Tu leva jeito pra demônio pena que o inferno táa lotado já."
+    ]
+
+    const numero = Math.floor(Math.random() * speak.length);
+
+    alert(speak[numero])
+
 }
 
 
 function talkTibine() {
-    alert("Eu to muito bêbada")
-    
+    const speak = ["Cara!!! Eu to muito bêbada!!",
+        "Viu meu amigo ?  Ele é pequeno, esquisito e verde.",
+        "Uma vez uma feira me disse que lógica e religião não combinam.",
+        "Nossa cara, sem querer ofender mas você é muito feio e olha que meu melhor amigo é um demônio."
+    ]
+
+    const numero = Math.floor(Math.random() * speak.length);
+
+    alert(speak[numero])
+
 }
 
 
 
 function talkElfo() {
-    alert("Alguém viu a tibine por aí?")
-    
+    const speak = ["Viu a Tibine por ai?"]
+
+    const numero = Math.floor(Math.random() * speak.length);
+
+    alert(speak[numero])
+
 }
